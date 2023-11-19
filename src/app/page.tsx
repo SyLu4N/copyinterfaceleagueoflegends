1aff884ba4739eb2e8583929f01265fe1a8b2eb8
Luan Simões <luaan.carlos@hotmail.com>
2023-11-19 04:14:04 -0300

    create home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { FaArrowRight } from 'react-icons/fa';
 
+import { Checkbox } from '@/components/checkbox';
+import { Input } from '@/components/input';
 import { Metadata } from 'next';
 
 import '../styles/globals.css';
-import { Input } from '@/components/input';
 
 export const metadata: Metadata = {
   title: 'Login - League of Legends',
@@ -11,34 +12,44 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="flex w-full max-w-4xl bg-neutral-100">
-      <div className="p-6 w-[30%] flex flex-col gap-4">
-        <h2>LOGO</h2>
+    <main className="flex w-full max-w-[1300px] bg-white">
+      <div className="p-10 min-w-[30%] flex flex-col gap-10">
+        <img
+          src="logo.png"
+          alt="Logotipo minificado do league of legends"
+          className="w-16 mt-2 mb-5 ml-[-12px]"
+        />
 
-        <h3 className="font-bold">Sign in with your Riot Account</h3>
+        <h2 className="font-bold text-2xl">Sign in with your Riot Account</h2>
 
-        <Input type="text" label="USERNAME" />
-        <input type="text" className="block" />
+        <div className="flex flex-col gap-2">
+          <Input label="USERNAME" />
+          <Input type="password" label="PASSWORD" />
 
-        <label className="flex gap-2">
-          <input type="checkbox" className="block" />
-          <p className="text-xs">Manter me conectado</p>
-        </label>
+          <Checkbox label="Stay signed in" />
+        </div>
 
         <div className="flex justify-center mt-4">
-          <button className="p-3 rounded-lg bg-red-600 text-white ">
+          <button className="p-5 rounded-2xl bg-red-600 text-white text-[30px] hover:bg-red-700 transition">
             <FaArrowRight />
           </button>
         </div>
 
-        <div className="text-[10px] mt-auto">
-          <p>CREATE ACCOUNT</p>
-          <p>CAN&apos;T SIGN IN </p>
-        </div>
+        <aside className="text-[12px] mt-auto text-gray-600 font-bold">
+          <p className="hover:text-red-600 cursor-pointer inline-block transition duration-300">
+            CREATE ACCOUNT
+          </p>
+
+          <br />
+
+          <p className="hover:text-red-600 cursor-pointer inline-block transition duration-300">
+            CAN&apos;T SIGN IN?
+          </p>
+        </aside>
       </div>
 
       <img
-        className="h-full w-[70%]"
+        className="h-full w-full"
         alt="Arte criada do league of legends"
         src="https://s2-techtudo.glbimg.com/tP2p4gidVIml_0b3vruDMMbYBbY=/0x0:695x431/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2017/M/o/ZAf7yxShGhGLmZXnZMdQ/splash.jpg"
       />
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,14 +1,39 @@
-import { InputHTMLAttributes } from 'react';
+/* eslint-disable prettier/prettier */
+'use client';
+
+import { InputHTMLAttributes, useState } from 'react';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
+  type?: string;
 }
 
-export function Input({ label, ...rest }: InputProps) {
+export function Input({ label, type = 'text', ...rest }: InputProps) {
+  const [isActive, setIsActive] = useState(false);
+
+  const [value, setValue] = useState('');
+
   return (
-    <div className="relative">
-      {label && <p className="absolute text-[10px] top-0">{label}</p>}
-      <input {...rest} />
+    <div
+      className={`relative flex items-center w-full  `}
+    >
+      {label && (
+        <label
+          className={`absolute  text-gray-600 font-bold left-1 transition-all ${isActive ? 'top-0 text-[8px]' : 'text-[12px] top-2'
+            } transition-all duration-300`}
+        >
+          {label}
+        </label>
+      )}
+
+      <input
+        {...rest}
+        type={type}
+        className="w-full pl-1 text-[16px] h-[40px] pt-1 font-bold bg-neutral-100"
+        onChange={(e) => setValue(e.target.value)}
+        onFocus={() => setIsActive(true)}
+        onBlur={() => setIsActive(!value ? false : true)}
+      />
     </div>
   );
 }
